23e2ceee27b7b80b7a5aa78e317ac927f010d207
Erasmo <prueba@pruebas-MacBook-Pro.local>
2020-11-08 05:48:50 -0400

    Add dependency to deploy on Heroku.

--- app.py
+++ app.py
@@ -6,7 +6,17 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route('/')
+def home():
+    return jsonify({'success': True})
+
+
 @app.route('/user/<username>')
 def get_person(username):
     response_proxy = requests.get(f'https://bio.torre.co/api/bios/{username}')
     return jsonify(response_proxy.json()) 
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
